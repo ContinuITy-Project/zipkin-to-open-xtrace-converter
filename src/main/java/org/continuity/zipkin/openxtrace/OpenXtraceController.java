01b89a126a015584c0cb1a90f4630432d980e022
Henning Schulz <henning.schulz@novatec-gmbh.de>
2019-03-14 18:15:16 +0100

    Added a REST endpoint for retrieving the OPEN.xtraces between a fromDate and endDate from a configured Zipkin host.

--- src/main/java/org/continuity/zipkin/openxtrace/OpenXtraceController.java
+++ src/main/java/org/continuity/zipkin/openxtrace/OpenXtraceController.java
@@ -1,14 +1,24 @@
 package org.continuity.zipkin.openxtrace;
 
+import static java.util.Comparator.comparingLong;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import org.continuity.zipkin.openxtrace.conversion.ZipkinConverter;
 import org.continuity.zipkin.openxtrace.data.ZipkinSpan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spec.research.open.xtrace.api.core.Trace;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +37,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RequestMapping("/open-xtrace")
 public class OpenXtraceController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(OpenXtraceController.class);
+
 	@Autowired
 	private RestTemplate restTemplate;
 
@@ -36,12 +48,64 @@ public class OpenXtraceController {
 	@Autowired
 	private ZipkinConverter converter;
 
+	@Value("${zipkin.base-url}")
+	private String zipkinBaseUrl;
+
+	@Value("${zipkin.limit-per-request}")
+	private int limitPerRequest;
+
 	@RequestMapping(path = "/get", method = RequestMethod.GET)
-	public List<Trace> getOpenXtraces() throws JsonParseException, JsonMappingException, IOException {
-		String json = restTemplate.getForObject("http://172.16.145.68:9411/api/v2/traces?limit=20", String.class);
+	public List<Trace> getOpenXtraces(Date fromDate, Date toDate) throws JsonParseException, JsonMappingException, IOException {
+		LOGGER.info("Retrieving and converting Zipkin traces between {} and {} in slices of max {} traces...", fromDate, toDate, limitPerRequest);
+
+		Set<Trace> traces = new TreeSet<>(comparingLong(Trace::getTraceId));
+
+		long endTs = toDate.getTime();
+		long startTs = fromDate.getTime();
+		long oldestTrace = 0;
+		long preOldestTrace = -1;
+
+		while ((endTs > startTs) && (oldestTrace != preOldestTrace)) {
+			Set<Trace> current = getAndConvertTraces(endTs, endTs - startTs);
+
+			if (current.size() > 0) {
+				Trace oldest = getOldest(current);
+				endTs = oldest.getRoot().getRoot().getTimestamp();
+
+				preOldestTrace = oldestTrace;
+				oldestTrace = oldest.getTraceId();
+
+				traces.addAll(current);
+			} else {
+				break;
+			}
+		}
+
+		List<Trace> traceList = new ArrayList<>(traces);
+		Collections.sort(traceList, comparingLong(this::getTimestamp));
+
+		return traceList;
+	}
+
+	private Set<Trace> getAndConvertTraces(long endTs, long lookback) throws JsonParseException, JsonMappingException, IOException {
+		String json = restTemplate.getForObject(zipkinBaseUrl + "/api/v2/traces?endTs=" + endTs + "&lookback=" + lookback + "&limit=" + limitPerRequest, String.class);
 		ZipkinSpan[][] traces = objectMapper.readValue(json, ZipkinSpan[][].class);
 
-		return Arrays.stream(traces).map(Arrays::asList).map(converter::convert).collect(Collectors.toList());
+		LOGGER.info("Got {} traces with endTs={} and lookback={}.", traces.length, endTs, lookback);
+
+		return Arrays.stream(traces).map(Arrays::asList).map(converter::convert).collect(Collectors.toSet());
+	}
+
+	private Trace getOldest(Set<Trace> traces) {
+		return traces.stream().reduce((a, b) -> getTimestamp(a) <= getTimestamp(b) ? a : b).get();
+	}
+
+	private long getTimestamp(Trace trace) {
+		if ((trace != null) && (trace.getRoot() != null) && (trace.getRoot().getRoot() != null)) {
+			return trace.getRoot().getRoot().getTimestamp();
+		} else {
+			return -1;
+		}
 	}
 
 }
--- src/main/java/org/continuity/zipkin/openxtrace/conversion/ZipkinConverter.java
+++ src/main/java/org/continuity/zipkin/openxtrace/conversion/ZipkinConverter.java
@@ -4,11 +4,8 @@ import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
-import java.io.PrintStream;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 import org.continuity.zipkin.openxtrace.data.ZipkinSpan;
 import org.continuity.zipkin.openxtrace.impl.ZipkinTraceImpl;
@@ -28,34 +25,7 @@ public class ZipkinConverter {
 				.collect(collectingAndThen(groupingBy(ZipkinSpan::getId),
 						map -> map.entrySet().stream().map(Entry::getValue).map(duplicatesResolver::resolve).flatMap(List::stream).collect(toList())));
 
-		// TODO: remove
-		convertRecursivley(resolved.stream().filter(span -> span.getParentId() == null).collect(Collectors.toList()), resolved, "");
-
 		return new ZipkinTraceImpl().fromZipkin(resolved);
-	}
-
-	private void convertRecursivley(List<ZipkinSpan> currentSpans, List<ZipkinSpan> trace, String indent) {
-		for (ZipkinSpan span : currentSpans) {
-			System.out.print(indent);
-			printSpan(span, System.out);
-
-			convertRecursivley(trace.stream().filter(s -> Objects.equals(s.getParentId(), span.getId())).collect(Collectors.toList()), trace, indent + "  ");
-		}
-	}
-
-	private void printSpan(ZipkinSpan span, PrintStream out) {
-		out.print(span.getKind());
-		out.print(" ");
-		out.print(span.getName());
-		out.print(" ");
-		out.print(span.getLocalEndpoint().getServiceName());
-		out.print(" ");
-		out.print(span.getTags().get("http.url"));
-		out.print(" d=");
-		out.print(span.getDuration());
-		out.print(" (");
-		out.print(span.getId());
-		out.println(")");
 	}
 
 }
